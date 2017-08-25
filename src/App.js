244a9b04cd9fed5c6613446e8dc6fdbd03f9f4c6
piotros <piotros@wp.pl>
2017-08-25 12:11:29 +0200

    Add first routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 import Main from './views/Main'
+import Details from "./views/Details"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main/>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact strict path="/" component={Main}/>
+          <Route exact strict path="/main" component={Main}/>
+          <Route exact strict path="/details/:id" component={Details}/>
+        </div>
+      </Router>
     )
   }
 }
