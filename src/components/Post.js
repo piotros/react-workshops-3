26726293fdebfe737def99b4bdb7068db1c41d84
piotros <piotros@wp.pl>
2017-08-25 12:30:43 +0200

    Add post detail

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Post = props => {
   const {id, title, body} = props.post
 
   return (
     <div className="post">
-      <h3>{id}: {title}</h3>
+      <Link to={`/details/${id}`}><h3>{id}: {title}</h3></Link>
       <p>{body}</p>
     </div>
   )
--- src/views/Details.js
+++ src/views/Details.js
@@ -1,10 +1,34 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {customFetch} from '../services/fetch'
+import Post from "../components/Post"
 
 class Details extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      post: {},
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    customFetch('https://jsonplaceholder.typicode.com/posts/' + this.props.match.params.id)
+      .then(response => response.json())
+      .then(post => this.setState({post, loading: false}))
+  }
+
   render() {
+    const {post, loading} = this.state
+
     return (
       <div className="details">
-        details
+        <Link to="/main">Back to main</Link>
+
+        <br/>
+
+        <Post post={post}/>
       </div>
     )
   }
